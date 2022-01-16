d2846ba29064b79075a67c04eb246ef63f09aaa3
veggiepilot <johanneschitura@gmail.com>
2022-01-15 23:53:29 -0500

    Added indexDB file

---
+++ public/indexDB.js
@@ -0,0 +1,1 @@
+const request = IndexedDB.open('EXAMPLE_DB', 1);
--- public/service-worker.js
+++ public/service-worker.js
@@ -1,8 +1,9 @@
-const CACHE_NAME = "static-cache-v1";
-const DATA_CACHE_NAME = "data-cache-v1";
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v2";
 const FILES_TO_CACHE = [
     "/",
-    "/index.html"
+    "/index.html",
+    "/styles.css"
 ];
 
 // install
